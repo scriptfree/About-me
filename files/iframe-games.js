6f75bab5f9171bb135b799f68c03e7f6a312e006
Manny <104465644+scriptfree@users.noreply.github.com>
2024-07-12 11:20:24 -0700

    Update iframe-games.js

--- files/iframe-games.js
+++ files/iframe-games.js
@@ -1,29 +1,31 @@
 javascript:(function() {
+    // List of supported game names
     var supportedGames = [
-
-"cookie clicker",
-"crossy road",
-"eggy car",
-"geometry dash",
-"gun mayhem 2",
-"krunker",
-"mad gunz",
-"moto x3m",
-"ovo",
-"roblox",
-"smash karts",
-"tap tap shots",
-"wings"
-        
+        "cookie clicker",
+        "crossy road",
+        "eggy car",
+        "geometry dash",
+        "gun mayhem 2",
+        "krunker",
+        "mad gunz",
+        "moto x3m",
+        "ovo",
+        "roblox",
+        "smash karts",
+        "tap tap shots",
+        "wings"
     ];
-    
-    var gameName = prompt("Type game name, dont add dashes (Press esc to cancel).");
+
+    var gameName = prompt("Type game name (Press esc to cancel).");
+
     if (gameName !== null) {
-        var linkfix = gameName.replace(/ /g, '-') + '.html';
-        
-        if (supportedGames.includes(gameName)) {
+        var lowerCaseGameName = gameName.toLowerCase(); // Convert to lowercase
+        var formattedName = lowerCaseGameName.replace(/ /g, '-') + '.html';
+
+        // Check if the entered game name is supported
+        if (supportedGames.includes(lowerCaseGameName)) {
             var iframe = document.createElement('iframe');
-            iframe.src = "https://raw.githubusercontent.com/uvgame/uvgame.github.io/main/" + linkfix;
+            iframe.src = "https://raw.githubusercontent.com/uvgame/uvgame.github.io/main/" + formattedName;
             iframe.style.position = "fixed";
             iframe.style.top = "0";
             iframe.style.left = "0";
@@ -32,7 +34,7 @@ javascript:(function() {
             iframe.style.border = "none";
             document.body.appendChild(iframe);
         } else {
-            alert("Game not supported");
+            alert("Game not supported.");
         }
     }
 })();
